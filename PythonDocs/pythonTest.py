088ed463d673d116c8fabfc350ca9bb5551d83eb
Xingzhe Zhao <samzhao0528@gmail.com>
2021-05-06 20:40:01 -0700

    new

--- PythonDocs/pythonTest.py
+++ PythonDocs/pythonTest.py
@@ -12,3 +12,20 @@ a = 2
 b = '2'
 print(sys.getsizeof(a))
 print(sys.getsizeof(b))
+
+hashT = {}
+hashT[1] = 1
+hashT[2] = 1
+print(hashT)
+
+ls = [1,2,3,4]
+t = [5]
+print(ls+t)
+print(t+ls)
+
+for i in range(9, -1, -1):
+    print(i)
+
+s = 'ABCDEDF'
+for index, value in enumerate(s):
+    print(index, value)
--- problems/reverseString.py
+++ problems/reverseString.py
@@ -2,16 +2,43 @@ def reverse_string_1(s):
     return ' '.join(reversed(s.split()))
 
 def reverse_string(s):
-    word = ''
-    sent = []
-    for i in s:
-        if i != ' ':
-            word += i
-        else:
-            sent.append(word)
-            word = ''
-    sent.append(word)
-    print(sent)
+    length = len(s)
+
+    words = list()
+    spaces = [' ']
+    i = 0
+    while i < length:
+        if s[i] not in spaces:
+            word_start = i
+            while i < length and i not in spaces:
+                i += 1
+            words.append(s[word_start:i])
+        i += 1
+    # print(f'show me i = {i} and coutner = {counter}')
+    return words
+    # while len(words) > 0:
+    #     temp = words.pop(-1)
+    #     print(temp)
+    #     result = result + ' ' + temp
+    # return result
+
 
 # print(reverse_string_1('Hello World this is Sam'))
-reverse_string('Hello World this is Sam')
+# print(reverse_string('Hello World this is Sam'))
+# st = []
+# lst = 'abcdefghi'
+# st.append(lst[0:2])
+# st.append(lst[2:4])
+# print(st)
+
+# def rev(s):
+#     return s.split()[::-1]
+
+# print(rev('Hello World this is Sam'))
+
+
+ls = [1,1,2]
+s = set()
+for i in ls:
+    s.add(i)
+print(len(s))
